f795fb6c99c7ee55016c507887d3a904782c6b17
Arthur Valentim Kasper <artule@gmail.com>
2021-03-02 21:17:36 -0300

    add-configuration-sleeper

--- Mock/mock.go
+++ Mock/mock.go
@@ -9,6 +9,13 @@ import (
 
 const finalWord = "Go!"
 const contDownStart = 3
+const write = "write"
+const sleep = "sleep"
+
+func (s *CountDownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 
@@ -21,7 +28,7 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleep{}
+	sleeper := &ConfigurableSleeper{2 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
 
@@ -51,10 +58,19 @@ func (s *CountDownOperationsSpy) Sleep() {
 	s.Calls = append(s.Calls, sleep)
 }
 
-func (s *CountDownOperationsSpy) Write(p []byte) (n int, err error) {
-	s.Calls = append(s.Calls, write)
-	return
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
 }
 
-const write = "write"
-const sleep = "sleep"
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
